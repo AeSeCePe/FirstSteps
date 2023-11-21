44c090d72bc0155ebce3348487a987da18233d15
AeSeCePe <U202210697@upc.edu.pe>
2023-11-20 19:19:03 -0500

    fix

--- Scripts/javascript.js
+++ Scripts/javascript.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const carouselSlide = document.querySelector('.carousel-slide');
   
     carouselSlide.addEventListener('click', function() {
-      window.location.href ="seguimiento.html"; // Reemplaza 'nueva_pagina.html' con la URL de la nueva página a la que deseas redirigir.
+      window.location.href ="./Public/seguimiento.html"; // Reemplaza 'nueva_pagina.html' con la URL de la nueva página a la que deseas redirigir.
     });
   });
   
